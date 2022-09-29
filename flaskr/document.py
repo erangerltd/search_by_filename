2d21f0631aef412185c466585c894c4fd91cb940
odaka <odaka@eranger.co.jp>
2022-09-29 14:49:25 +0900

    金額での検索を追加

--- flaskr/document.py
+++ flaskr/document.py
@@ -4,11 +4,13 @@
     t.odaka
 
 '''
+#from lib2to3.pytree import _Results
+from curses.ascii import isdigit
 import os
 import urllib
 import datetime, re
 from dateutil.relativedelta import relativedelta
-from pickle import DICT
+from pickle import DICT, FALSE
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for, current_app, make_response
 )
@@ -46,8 +48,6 @@ def date_format_check(date_fmt):
     if result:
         y, m, d = result.groups()
 #        current_app.logger.debug(y)
-#        current_app.logger.debug(m)
-#        current_app.logger.debug(d)
         if y < "2021":
             return False
         if m > "12":
@@ -56,13 +56,28 @@ def date_format_check(date_fmt):
             return False
         
         _dt = y+m+d
-#        current_app.logger.debug(_dt)
         if len(_dt) > 8:
             return False
     
         return _dt
     else:
         return False
+
+##
+#  ファイル名から金額を抜き出す
+# 
+##
+def get_amount(filename):
+    a_pattern = re.compile('\d*')
+    results = a_pattern.findall(filename)
+#    current_app.logger.debug(results)
+
+    for _r in results:
+        if len(_r) > 0 and not date_format_check(_r):
+            return str(_r)
+    
+    return False
+
 
 ##
 #  処理日の何ヶ月か前の１日目を取得する
@@ -73,6 +88,119 @@ def get_firstday_month(_mbefore):
     thismonth = datetime.datetime(today.year, today.month, 1)
     targetmonth = thismonth - relativedelta(months=int(_mbefore))
     return targetmonth.strftime('%Y%m%d')
+
+##
+#  データリストの作成
+#
+##
+def create_datalist(word, sdate, edate, samount, eamount):
+    _l = list()
+    for _t in current_app.config["DOC_PATH"]:
+        # https://www.curict.com/item/48/4807c14.html
+        for root, dirs, files in os.walk(top=_t):
+            for file in files:
+                if not file.startswith(('.')) \
+                    and file.endswith(tuple(current_app.config["DOC_FORMAT"])) \
+                    and (current_app.config["DOC_KEY"] in file):
+
+                    # ファイル名から日付と金額を抽出
+                    _fd = False
+                    _fa = False
+                    if date_format_check(file):
+                        _fd = date_format_check(file)
+                    if get_amount(file):
+                        _fa = get_amount(file)
+
+                    # 日付と金額がともに抽出できなければ対象外
+                    _er = True
+                    if not _fd or not _fa:
+                        _er = False
+
+                    # 期間開始日以降のファイル
+#                   current_app.logger.debug(date_format_check(file))
+                    if _er:
+                        current_app.logger.debug(file)
+                        current_app.logger.debug(_fa)
+                        current_app.logger.debug(samount)
+                        if _fd >= sdate:
+                            # 期間終了日が指定された場合
+                            if len(edate) > 0:
+                                if _fd <= edate:
+                                    if int(_fa) >= int(samount):
+                                        # 金額範囲（上限）が指定されている場合
+                                        if len(eamount) > 0:
+                                            if int(_fa) <= int(eamount):
+                                                # フリーワードが指定された場合
+                                                _r = None
+                                                if not word is None:
+                                                    if word in file:
+                                                        file_path = os.path.join(root, file)
+                                                        _r = get_file_attr(file_path)
+                                                    else:
+                                                        pass
+                                                else:
+                                                    file_path = os.path.join(root, file)
+                                                    _r = get_file_attr(file_path)
+
+                                                if _r is not None:
+                                                    _l.append( _r )
+
+                                        # 金額範囲（上限）が指定されない場合
+                                        else:
+                                            # フリーワードが指定された場合
+                                            _r = None
+                                            if not word is None:
+                                                if word in file:
+                                                    file_path = os.path.join(root, file)
+                                                    _r = get_file_attr(file_path)
+                                                else:
+                                                    pass
+                                            else:
+                                                file_path = os.path.join(root, file)
+                                                _r = get_file_attr(file_path)
+
+                                            if _r is not None:
+                                                _l.append( _r )
+
+                            # 期間終了日が指定されない場合
+                            else:
+                                if int(_fa) >= int(samount):
+                                    # 金額範囲（上限）が指定されている場合
+                                    if len(eamount) > 0:
+                                        if int(_fa) <= int(eamount):
+                                            # フリーワードが指定された場合
+                                            _r = None
+                                            if not word is None:
+                                                if word in file:
+                                                    file_path = os.path.join(root, file)
+                                                    _r = get_file_attr(file_path)
+                                                else:
+                                                    pass
+                                            else:
+                                                file_path = os.path.join(root, file)
+                                                _r = get_file_attr(file_path)
+
+                                            if _r is not None:
+                                                _l.append( _r )
+
+                                    # 金額範囲（上限）が指定されない場合
+                                    else:
+                                        # フリーワードが指定された場合
+                                        _r = None
+                                        if not word is None:
+                                            if word in file:
+                                                file_path = os.path.join(root, file)
+                                                _r = get_file_attr(file_path)
+                                            else:
+                                                pass
+                                        else:
+                                            file_path = os.path.join(root, file)
+                                            _r = get_file_attr(file_path)
+
+                                        if _r is not None:
+                                            _l.append( _r )
+    
+    return _l
 
 
 ##
@@ -88,11 +216,14 @@ def index():
 
     # 入力チェック
     edate = ''
+    eamount = ''
     word = ''
     if request.method == 'POST':
         word = request.form['word']
         sdate = request.form['sdate']
         edate = request.form['edate']
+        samount = request.form['samount']
+        eamount = request.form['eamount']
         if len(sdate) > 0:
             if not date_format_check(sdate):
                 _em = "検索開始日が正しくありません"
@@ -103,57 +234,33 @@ def index():
         if len(_em) == 0:
             if len(edate) > 0 and edate < sdate:
                 _em = "検索終了日は開始日以降にしてください"
+        if len(_em) == 0:
+            if len(samount) > 0:
+                if not samount.isdecimal():
+                    _em = "金額蘭には数字を入力してください"
+        if len(_em) == 0:
+            if len(eamount) > 0:
+                if not eamount.isdecimal():
+                    _em = "金額蘭には数字を入力してください"
+        if len(_em) == 0:
+            if len(samount) > 0 and len(eamount) > 0:
+                if samount > eamount:
+                    _em = "金額範囲の指定が正しくありません"
+
     else:
         # GETの場合には絞り込み開始日の初期値をセット。（初回照会時）
         sdate = get_firstday_month(2)
+        samount = '0'
 #        dt_now = datetime.datetime.now()
 #        sdate = dt_now.strftime('%Y%m%d')
 #        current_app.logger.debug(sdate)
 
     if len(_em) == 0:
-        for _t in current_app.config["DOC_PATH"]:
-            # https://www.curict.com/item/48/4807c14.html
-            for root, dirs, files in os.walk(top=_t):
-                for file in files:
-                    if not file.startswith(('.')) \
-                        and file.endswith(tuple(current_app.config["DOC_FORMAT"])) \
-                        and (current_app.config["DOC_KEY"] in file):
-                        # 開始期間以降のファイル
-                        if sdate <= date_format_check(file):
-                            if len(edate) > 0:
-                                if edate >= date_format_check(file):
-                                    # フリーワードが指定された場合
-                                    _r = None
-                                    if not word is None:
-                                        if word in file:
-                                            file_path = os.path.join(root, file)
-                                            _r = get_file_attr(file_path)
-                                        else:
-                                            pass
-                                    else:
-                                        file_path = os.path.join(root, file)
-                                        _r = get_file_attr(file_path)
-
-                                    if _r is not None:
-                                        _l.append( _r )
-                            else:
-                                # フリーワードが指定された場合
-                                _r = None
-                                if not word is None:
-                                    if word in file:
-                                        file_path = os.path.join(root, file)
-                                        _r = get_file_attr(file_path)
-                                    else:
-                                        pass
-                                else:
-                                    file_path = os.path.join(root, file)
-                                    _r = get_file_attr(file_path)
-
-                                if _r is not None:
-                                    _l.append( _r )
+        # データリストの作成
+        _l = create_datalist(word, sdate, edate, samount, eamount)
 
 
-    return render_template('document/index.html', paths=_l, emsg=_em, sdate=sdate)
+    return render_template('document/index.html', paths=_l, emsg=_em, sdate=sdate, samount=samount)
 
 
 ##
@@ -186,7 +293,6 @@ def download(file_path):
 @bp.route('/csv', methods=['POST'])
 @login_required
 def dlcsv():
-    word = None
     _em = ''
     _l = list()
 
@@ -194,6 +300,8 @@ def dlcsv():
     word = request.form['word']
     sdate = request.form['sdate']
     edate = request.form['edate']
+    samount = request.form['samount']
+    eamount = request.form['eamount']
     if len(sdate) > 0:
         if not date_format_check(sdate):
             _em = "検索開始日が正しくありません"
@@ -204,53 +312,28 @@ def dlcsv():
     if len(_em) == 0:
         if len(edate) > 0 and edate < sdate:
             _em = "検索終了日は開始日以降にしてください"
+    if len(_em) == 0:
+        if len(samount) > 0:
+            if not samount.isdecimal():
+                _em = "金額蘭には数字を入力してください"
+    if len(_em) == 0:
+        if len(eamount) > 0:
+            if not eamount.isdecimal():
+                _em = "金額蘭には数字を入力してください"
+    if len(_em) == 0:
+        if len(samount) > 0 and len(eamount) > 0:
+            if samount > eamount:
+                _em = "金額範囲の指定が正しくありません"
 
     if len(_em) > 0:
         return render_template('document/index.html', paths=_l, emsg=_em)
 
     else:
-        for _t in current_app.config["DOC_PATH"]:
-            # https://www.curict.com/item/48/4807c14.html
-            for root, dirs, files in os.walk(top=_t):
-                for file in files:
-                    if not file.startswith(('.')) \
-                        and file.endswith(tuple(current_app.config["DOC_FORMAT"])) \
-                        and (current_app.config["DOC_KEY"] in file):
-                        # 開始期間以降のファイル
-                        if sdate <= date_format_check(file):
-                            if len(edate) > 0:
-                                if edate >= date_format_check(file):
-                                    # フリーワードが指定された場合
-                                    _r = None
-                                    if not word is None:
-                                        if word in file:
-                                            file_path = os.path.join(root, file)
-                                            _r = get_file_attr(file_path)
-                                        else:
-                                            pass
-                                    else:
-                                        file_path = os.path.join(root, file)
-                                        _r = get_file_attr(file_path)
+        # データリストの作成
+        _l = create_datalist(word, sdate, edate, samount, eamount)
 
-                                    if _r is not None:
-                                        _l.append( _r )
-                            else:
-                                # フリーワードが指定された場合
-                                _r = None
-                                if not word is None:
-                                    if word in file:
-                                        file_path = os.path.join(root, file)
-                                        _r = get_file_attr(file_path)
-                                    else:
-                                        pass
-                                else:
-                                    file_path = os.path.join(root, file)
-                                    _r = get_file_attr(file_path)
-
-                                if _r is not None:
-                                    _l.append( _r )
         # csvデータの作成
-#        https://teratail.com/questions/59915
+#       https://teratail.com/questions/59915
 #       Shift-jisに変換するので面倒臭い感じになっている。        
         _sio = StringIO()
         _w = csv.writer(_sio)
@@ -266,6 +349,7 @@ def dlcsv():
 
 
         return _res
+
 
 ##
 #   config.jsonの内容を表示する
